70a1f697a37d1341027adf45ace725f16fb470bf
AdarshMitr <adarshkumar019@gmail.com>
2023-09-22 13:32:19 +0530

    toggle in hamburger menu

--- src/components/Head.js
+++ src/components/Head.js
@@ -2,25 +2,38 @@ import React from "react";
 import hamburger from "../img/hamburger.png";
 import logo from "../img/logo.png";
 import user from "../img/user-icon.png";
+import { useDispatch } from "react-redux";
+import { toggleMenu } from "../utils/appSlice";
 
 const Head = () => {
+  const dispatch=useDispatch();
+
+  const toggleMenuHandler=()=>{
+    dispatch(toggleMenu());
+  }
   return (
     <div className="grid grid-flow-col  p-5 shadow-lg">
       <div className="flex col-span-1">
-        <img className="h-8 " alt="menu" src={hamburger} />
-        <img className="h-8 mx-2" alt="logo" src={logo} />
+        <img onClick={()=>toggleMenuHandler()}
+         className="h-8 cursor-pointer" alt="menu" src={hamburger} /> 
+         <a href="/">
+         <img className="h-8 mx-2" alt="logo" src={logo} />
+         </a>
+        
       </div>
 
       <div className="col-span-10 px-12 ">
-        <input className="w-1/2 border border-gray-400 p-2 rounded-l-full" type="text" />
+        <input
+          className="w-1/2 border border-gray-400 p-2 rounded-l-full"
+          type="text"
+        />
         <button className="border border-gray-600 p-2 rounded-r-full">
-        🔍
+          🔍
         </button>
       </div>
       <div>
         <img className="h-8" alt="user" src={user} />
       </div>
-
     </div>
   );
 };
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import { useSelector } from 'react-redux';
+
 
 const Sidebar = () => {
+  const isMenuOpen=useSelector((store)=>store.app.isMenuOpen);
+  //early return pattern
+  if(!isMenuOpen) return null;
   return (
-    <div className='p-5 shadow-lg w-[250px]'>
+    <div className='p-5 shadow-lg w-48'>
        <ul>
 <li>Home</li>
 <li>Shorts</li>
