bdc3b0e67206009858d631bb4126ad7d7b603518
AdarshMitr <adarshkumar019@gmail.com>
2023-09-22 13:11:14 +0530

    set up store

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@
 import "./App.css";
 import Head from './components/Head';
 import Body from "./components/Body";
+import store from "./utils/store";
+import { Provider } from "react-redux";
+
 function App() {
   return (
-    <div>
+    <Provider store={store}>
+ <div>
   <Head/>
   <Body/>
     </div>
+    </Provider>
+   
   
   );
 }
