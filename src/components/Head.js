591be2ad9d95d7fac3ab67d317f40d757b56d793
AdarshMitr <adarshkumar019@gmail.com>
2023-09-22 12:01:27 +0530

    created Header

--- src/components/Head.js
+++ src/components/Head.js
@@ -5,19 +5,22 @@ import user from "../img/user-icon.png";
 
 const Head = () => {
   return (
-    <div className="grid grid-flow-col">
-      <div className="flex">
-        <img className='h-8' alt="menu" src={hamburger} />
-        <img className="h-8" alt="logo" src={logo} />
+    <div className="grid grid-flow-col  p-5 shadow-lg">
+      <div className="flex col-span-1">
+        <img className="h-8 " alt="menu" src={hamburger} />
+        <img className="h-8 mx-2" alt="logo" src={logo} />
       </div>
 
-      <div>
-        <input type="text" />
-        <button>Search</button>
+      <div className="col-span-10 px-12 ">
+        <input className="w-1/2 border border-gray-400 p-2 rounded-l-full" type="text" />
+        <button className="border border-gray-600 p-2 rounded-r-full">
+        🔍
+        </button>
       </div>
       <div>
         <img className="h-8" alt="user" src={user} />
       </div>
+
     </div>
   );
 };
