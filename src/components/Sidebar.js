c921c05d0b951a3b9e7e74cc88ed21551b1e2261
AdarshMitr <adarshkumar019@gmail.com>
2023-09-26 12:14:44 +0530

    routes set up

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,15 +1,16 @@
 import React from 'react'
 import { useSelector } from 'react-redux';
-
+import { Link } from 'react-router-dom';
 
 const Sidebar = () => {
   const isMenuOpen=useSelector((store)=>store.app.isMenuOpen);
   //early return pattern
   if(!isMenuOpen) return null;
   return (
-    <div className='p-5 shadow-lg w-60'>
+    <div className='p-5 shadow-lg w-45'>
        <ul>
-<li>Home</li>
+<li><Link to="/">Home
+</Link></li>
 <li>Shorts</li>
 <li>Videos</li>  
 <li>Live</li>
